39593e1d283ab510b9c4f28262d09a800af2447e
Ian Davis <idavis@innovatian.com>
2010-03-19 23:18:19 -0700

    Removing logging, updating license, adding build scripts.

--- source/Ninject.Web/KernelContainer.cs
+++ source/Ninject.Web/KernelContainer.cs
@@ -1,21 +1,12 @@
 #region License
 
-//
-// Author: Nate Kohari <nkohari@gmail.com>
-// Copyright (c) 2007-2009, Enkari, Ltd.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
+// 
+// Author: Nate Kohari <nate@enkari.com>
+// Copyright (c) 2007-2010, Enkari, Ltd.
+// 
+// Dual-licensed under the Apache License, Version 2.0, and the Microsoft Public License (Ms-PL).
+// See the file LICENSE.txt for details.
+// 
 
 #endregion
 
@@ -70,9 +61,9 @@ namespace Ninject.Web
             if ( _kernel == null )
             {
                 throw new InvalidOperationException( String.Format(
-                                                         "The type {0} requested an injection, but no kernel has been registered for the web application.\r\n" +
-                                                         "Please ensure that your project defines a NinjectHttpApplication.",
-                                                         instance.GetType() ) );
+                    "The type {0} requested an injection, but no kernel has been registered for the web application.\r\n" +
+                    "Please ensure that your project defines a NinjectHttpApplication.",
+                    instance.GetType() ) );
             }
 
             _kernel.Inject( instance );
