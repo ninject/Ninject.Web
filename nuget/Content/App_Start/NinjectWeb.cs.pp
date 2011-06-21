b02a19c35254b01f5acadadf070e2ea37843a2b2
Remo Gloor <remo.gloor@bbv.ch>
2011-06-21 16:11:33 +0200

    Added AppStart to NuGet to register the NinjectWebModule

---
+++ nuget/Content/App_Start/NinjectWeb.cs.pp
@@ -0,0 +1,19 @@
+﻿[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.NinjectWeb), "Start")]
+
+namespace $rootnamespace$.App_Start
+{
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject.Web;
+
+    public static class NinjectWeb 
+    {
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start() 
+        {
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+        }
+    }
+}
